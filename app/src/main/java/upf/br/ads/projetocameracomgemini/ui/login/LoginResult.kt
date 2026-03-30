c2647c1e3f0703c21aad7e43f3844128ea0dd8fb
PessattoPadilha <gustavopessatto1919@gmail.com>
2026-03-30 20:18:46 -0300

    tela de login

---
+++ app/src/main/java/upf/br/ads/projetocameracomgemini/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package upf.br.ads.projetocameracomgemini.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
